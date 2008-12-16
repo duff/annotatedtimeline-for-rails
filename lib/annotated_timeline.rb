aef8ec3d2be5f94672d56b932f7fc46d1086cf21
Jeff Latz <jeff8448@yahoo.com>
2008-12-16 10:47:25 -0500

    annotations work title form only and i must test this code soon

--- lib/annotated_timeline.rb
+++ lib/annotated_timeline.rb
@@ -2,35 +2,44 @@ module AnnotatedTimeline
 
   def annotated_timeline(daily_counts_by_type, width = 750, height = 300, element = 'graph', options = {})
   
-      html = "<script type=\"text/javascript\" src=\"http://www.google.com/jsapi\"></script>\n<script type=\"text/javascript\"> \n"
-      html += "google.load(\"visualization\", \"1\", {packages:[\"annotatedtimeline\"]}); \n"  
-      html += "google.setOnLoadCallback(drawChart);"    
-      html += "function drawChart(){"  
-    	html += "var data = new google.visualization.DataTable(); \n"
-  		html += google_graph_data(daily_counts_by_type)
-  		html += "var chart = new google.visualization.AnnotatedTimeLine(document.getElementById(\'#{element}\')); \n"
-  		html += "chart.draw(data"   
-		
+    html = "<script type=\"text/javascript\" src=\"http://www.google.com/jsapi\"></script>\n<script type=\"text/javascript\"> \n"
+    html += "google.load(\"visualization\", \"1\", {packages:[\"annotatedtimeline\"]}); \n"  
+    html += "google.setOnLoadCallback(drawChart);"    
+    html += "function drawChart(){"  
+    html += "var data = new google.visualization.DataTable(); \n"
+    html += google_graph_data(daily_counts_by_type, options)
+    html += "var chart = new google.visualization.AnnotatedTimeLine(document.getElementById(\'#{element}\')); \n"
+    html += "chart.draw(data"   
 
-  		  if options[:zoomEndTime]
-  		    options[:zoomEndTime] = "new Date(#{options[:zoomEndTime].year}, #{options[:zoomEndTime].month-1}, #{options[:zoomEndTime].day})"
-  	    end
-  	    if options[:zoomStartTime]
-  	      options[:zoomStartTime] = "new Date(#{options[:zoomStartTime].year}, #{options[:zoomStartTime].month-1}, #{options[:zoomStartTime].day})"
-        end
-        if options[:colors]
-          options[:colors] = "#{options[:colors].inspect}"
-        end
-  		  array = options.map{|key,val| key.to_s + ": " + val.to_s}
-  		  html += (", {" + array.join(", ") + "}") unless array.empty?
-		
-  		html += "); } \n"		
-  		html += "</script>"
-  		html +=	"<div id=\"#{element}\" style=\"width: #{width}px\; height: #{height}px\;\"></div>"
-      html
+
+    if options[:zoomEndTime]
+        options[:zoomEndTime] = "new Date(#{options[:zoomEndTime].year}, #{options[:zoomEndTime].month-1}, #{options[:zoomEndTime].day})"
+    end
+    
+    if options[:zoomStartTime]
+        options[:zoomStartTime] = "new Date(#{options[:zoomStartTime].year}, #{options[:zoomStartTime].month-1}, #{options[:zoomStartTime].day})"
+    end
+    
+    if options[:colors]
+        options[:colors] = "#{options[:colors].inspect}"
+    end
+  	
+  	if options[:annotations]
+  	    options[:displayAnnotations] = true
+    end
+  	
+
+    array = options.delete_if{|k,v| k == :annotations}.map{|key,val| key.to_s + ": " + val.to_s}
+    html += (", {" + array.join(", ") + "}") unless array.empty?
+
+    html += "); } \n"		
+    html += "</script>"
+    html +=	"<div id=\"#{element}\" style=\"width: #{width}px\; height: #{height}px\;\"></div>"
+    html
+
   end
 
-  def google_graph_data(daily_counts_by_type)
+  def google_graph_data(daily_counts_by_type, options)
     length = daily_counts_by_type.values.size
     column_index = {}
     num = 0
@@ -41,6 +50,17 @@ module AnnotatedTimeline
     daily_counts_by_type.values.inject(&:merge).stringify_keys.keys.sort.each do |type,count|
   		html+="data.addColumn('number', '#{type.to_s.titleize}');\n"
   		column_index[num+=1] = type.to_sym
+  		
+  		if options[:annotations]
+      		if options[:annotations].keys.include?(type.to_sym)
+      		    html+="data.addColumn('string', '#{type.to_s.titleize}_annotation_title');\n"
+      		    column_index[num+=1] = (type.to_s + "_annotation_title").to_sym
+      		    options[:annotations][type.to_sym].each do |date,txt|
+      		        daily_counts_by_type[date][(type.to_s + "_annotation_title").to_sym] = "\"#{txt}\""
+      		    end
+    	    end
+	    end
+	    
   	end    
     
     html+="data.addRows(#{length});\n"
