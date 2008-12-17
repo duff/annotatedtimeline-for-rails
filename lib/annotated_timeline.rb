bb30661930b98f609d820754c1503f0fdba5a7c5
Jeff Latz <jeff8448@yahoo.com>
2008-12-17 13:47:38 -0500

    documented the 'inline' function's differences to the normal one

--- lib/annotated_timeline.rb
+++ lib/annotated_timeline.rb
@@ -1,10 +1,13 @@
 module AnnotatedTimeline 
-    
+
+    #this version will automatically create a div inline for you. 
+    #note that if it is in the middle of the page, the javascript will try to execute before the page load completes
   def inline_annotated_timeline(daily_counts_by_type, width = 750, height = 300, div_id_to_create = 'graph', options = {})
     html = annotated_timeline(daily_counts_by_type, div_id_to_create, options)
     html +=	"<div id=\"#{div_id_to_create}\" style=\"width: #{width}px\; height: #{height}px\;\"></div>"    
   end
 
+  #you must create a div on your page and pass in the div id
   def annotated_timeline(daily_counts_by_type, div_id = 'graph', options = {})
     html = "<script type=\"text/javascript\" src=\"http://www.google.com/jsapi\"></script>\n<script type=\"text/javascript\"> \n"
     html += "google.load(\"visualization\", \"1\", {packages:[\"annotatedtimeline\"]}); \n"  
